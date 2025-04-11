f41e78a0486b84d85546fd2b73f1dbb6ef174f91
Ar5h1a-M <2542915@students.wits.ac.za>
2025-04-11 18:10:13 +0200

    Fixed Redirect issue

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -11,8 +11,8 @@ export default function LoginPage() {
   // Function to handle Microsoft login
   const handleMicrosoftLogin = () => {
     try{
-    const clientId = process.env.AZURE_CLIENT_ID!;
-    const tenantId = process.env.AZURE_TENANT_ID!;
+    const clientId = process.env.NEXT_PUBLIC_AZURE_AZURE_CLIENT_ID!;
+    const tenantId = process.env.NEXT_PUBLIC_AZURE_AZURE_TENANT_ID!;
     const redirectUri = process.env.NEXT_PUBLIC_AZURE_REDIRECT_URI!;
     const scope = 'openid profile email offline_access User.Read';
     console.log("Redirect URI:", process.env.NEXT_PUBLIC_AZURE_REDIRECT_URI);
@@ -38,23 +38,25 @@ export default function LoginPage() {
       const interval = setInterval(async() => {
         if (authWindow.closed) {
           clearInterval(interval);
-          const code = new URLSearchParams(window.location.search).get('code');
+          /*const code = new URLSearchParams(window.location.search).get('code');*/
+          const hashParams = new URLSearchParams(window.location.hash.substring(1));
+          const token = hashParams.get('access_token');
 
-          if (code) {
+          if (token) {
             // 2. Exchange the code for an access token (you may need to send this code to your backend)
             const response = await fetch('/api/auth/microsoft', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
               },
-              body: JSON.stringify({ code }),
+              body: JSON.stringify({ token }),
             });
 
             const data = await response.json();
             if (data.token) {
               // 3. Store the JWT and handle user redirection
-              localStorage.setItem('jwt', data.token); // or store in a global state
-              router.push('/login'); // or wherever you want to redirect
+              localStorage.setItem('jwt', data.token); 
+              router.push('/login'); 
             } else {
               alert('Error: ' + data.error);
             }
