e43333b3ce99971dcd58dee42d42079f4c74ecb8
Connor Young <2549142@students.wits.ac.za>
2025-04-13 17:57:55 +0200

    More testing

--- backend/ThinkSync-backend-API/server.js
+++ backend/ThinkSync-backend-API/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
 const db = require("./db");
-// const cors = require("cors");
+const cors = require("cors");
 const helmet = require("helmet");
 
 dotenv.config();
@@ -14,9 +14,9 @@ app.use(express.json());
 
 app.use(helmet());
 
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN
-// }));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}));
 
 app.use('/api/auth', authRoutes);
 const PORT = process.env.PORT || 3000;
